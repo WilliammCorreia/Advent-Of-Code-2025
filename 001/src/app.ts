97d4651fe51d34927858b202b5e7a52f18210a23
WilliammCorreia <william.correia.pro@outlook.com>
2025-12-01 23:15:54 +0100

    day 1 : gold star

--- 001/src/app.ts
+++ 001/src/app.ts
@@ -4,34 +4,42 @@ const fileUtils = new FileUtils();
 
 const main = async (): Promise<void> => {
     let dial: number = 50;
-    let counter: number = 0;
-    const lines: String[] = await fileUtils.readAttachedDocument('attached_document.txt');
+    let zeroCounter: number = 0;
+    let rotationCounter: number = 0;
+    const lines: string[] = await fileUtils.readAttachedDocument('attached_document.txt');
 
     for (const line of lines) {
-        const isLeft: boolean = line[0] === "L" ? true : false;
-        let rotation: number = parseInt(line.slice(1));
+        const isLeft: boolean = line.startsWith('L');
+        let rotation: number = parseInt(line.slice(1), 10);
 
-        if (rotation > 100) {
-            const centil: number = Math.floor(rotation / 100);
-            rotation -= centil * 100;
+        if (rotation >= 100) {
+            const hundredth: number = Math.floor(rotation / 100);
+            rotationCounter += hundredth;
+            rotation -= hundredth * 100;
         }
 
         if (isLeft) {
             if (dial - rotation < 0) {
+                if (dial !== 0) {
+                    rotationCounter++;
+                }
+
                 dial = 100 + dial - rotation;
             } else {
                 dial -= rotation;
 
-                if (dial === 0) {
-                    counter++;
+                if (dial === 0 && rotation !== 0) {
+                    zeroCounter++;
+                    rotationCounter++;
                 }
             }
         } else {
             if (dial + rotation > 99) {
                 dial = dial + rotation - 100;
+                rotationCounter++;
                 
                 if (dial === 0) {
-                    counter++;
+                    zeroCounter++;
                 }
             } else {
                 dial += rotation;
@@ -39,7 +47,8 @@ const main = async (): Promise<void> => {
         }
     }
 
-    console.log(`Dial left pointed at 0 : ${counter} times.`)
+    console.log(`Dial left pointed at 0 : ${zeroCounter} times.`)
+    console.log(`Dial pointed at 0 : ${rotationCounter} times.`)
 };
 
 main();
--- 001/src/file.utils.ts
+++ 001/src/file.utils.ts
@@ -1,8 +1,8 @@
 import { readFile } from 'fs/promises';
 
 export class FileUtils {
-    readAttachedDocument = async (path: string): Promise<String[]> => {
+    readAttachedDocument = async (path: string): Promise<string[]> => {
         const file = await readFile(path, 'utf-8');
-        return file.split('\r\n');
+        return file.split(/\r?\n/);
     }
 }
